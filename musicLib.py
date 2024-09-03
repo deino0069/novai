adc7b4bbf1b13dce49a48bf24e9e779a3ff793b8
deino0069 <163132919+deino0069@users.noreply.github.com>
2024-09-03 16:24:50 +0530

    musicLib.py

---
+++ musicLib.py
@@ -0,0 +1,5 @@
+music  = {"songs": "https://www.youtube.com/watch?v=cWMxCE2HTag&list=PLNCA1T91UH31M7mN8iKSxMwwWB_mkzwT6",
+          "song" :  "https://youtu.be/n_FCrCQ6-bA?feature=shared",
+          "music": "https://youtu.be/ElZfdU54Cp8?feature=shared",
+          "big" : "https://youtu.be/hOHKltAiKXQ?feature=shared",
+          "baby" : "https://youtu.be/kffacxfA7G4?feature=shared"}
